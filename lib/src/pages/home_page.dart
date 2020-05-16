700cdd05192fb9ac4d75b8a3179f8af5eed74798
Antonio Ayala <antonio.ayala.a@gmail.com>
2020-05-15 22:00:35 -0400

    Creando Base de Datos SQLite

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+
 import 'package:qrreader_app/src/pages/direcciones_page.dart';
 import 'package:qrreader_app/src/pages/mapas_page.dart';
+
+import 'package:barcode_scan/barcode_scan.dart';
+
 
 
 class HomePage extends StatefulWidget {
@@ -10,14 +14,43 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  int currentIndex = 1;
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _cargarPage(currentIndex),
+      appBar: AppBar(
+        title: Text('QR Scanner'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: (){}
+          )
+        ],
+      ),
+      body: _callPage(currentIndex),
       bottomNavigationBar: _crearBottomNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.filter_center_focus),
+        onPressed: _scanQR(),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+
     );
+  }
+
+  _scanQR() async {
+    //https://www.zentag.io
+    //geo: -17.784161988650467, -63.179601578320344
+    dynamic futureString = '';
+    // try {
+    //   futureString = await BarcodeScanner.scan();
+    // } catch (e) {
+    //   futureString = e.toString();
+    // }
+    // print('Future String:$futureString');
+
   }
 
   Widget _crearBottomNavigationBar () {
@@ -37,12 +70,12 @@ class _HomePageState extends State<HomePage> {
         BottomNavigationBarItem(
           icon: Icon(Icons.brightness_5),
           title: Text('Direcciones')
-        )
+        ),
       ],
     );
   }
 
-  Widget _cargarPage( int paginaActual ){
+  Widget _callPage( int paginaActual ){
 
     switch( paginaActual){
       case 0 : return MapasPage();
